187a6d07c95ee84b94d71e74e9bce08825d0f1a7
dosuxz <ritabandas2000@gmail.com>
2020-10-21 23:13:37 -0700

    Added the code and updated README

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Pretty-Shitty-Proxy
 This is a proxy server written in golang. I'll use this for malware analysis and for analysing the requests.
+
+## Usage 
+
+```
+go run psp.go
+```
+
+The proxy will start running on port 1331. Set your browsers or some other device's port to 1331. 
+
+**Note** : It currently does not have support for HTTPS. 
---
+++ psp.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	ln, err := net.Listen("tcp", ":1331")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("[*] Server started at port 1331")
+	defer ln.Close()
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("[*] Connection accepted....")
+		data := make([]byte, 8192)
+		_, err = conn.Read(data)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(data))
+		fmt.Println(reflect.TypeOf(conn))
+
+		go connString(conn, data)
+	}
+}
+
+func connString(conn net.Conn, data []byte) {
+	temp := strings.Split(string(data), "\n")
+	fmt.Println("Splitlines : ", len(temp))
+	fmt.Println("Of tyep : ", reflect.TypeOf(temp))
+	fmt.Println("Printing the array...")
+	var addr string
+	var tt string
+	var port int
+
+	for i := 0; i < len(temp); i++ {
+		if strings.Contains(temp[i], "Host: ") {
+			tt = temp[i][6:(len(temp[i]) - 1)]
+			fmt.Println(tt)
+			fmt.Println(strings.Contains(tt, ":"))
+			break
+		}
+	}
+	if strings.Contains(tt, ":") {
+		addr = strings.Split(tt, ":")[0]
+		port, _ = strconv.Atoi(strings.Split(tt, ":")[1])
+		fmt.Println("PORT : ", port)
+		fmt.Println(addr)
+	} else {
+		addr = tt
+		port = 80
+	}
+	ipAddr, err := net.LookupIP(string(addr))
+	fmt.Println("IP Addres is :", ipAddr[0].String())
+	if err != nil {
+		panic(err)
+	}
+
+	proxyThisCunt(ipAddr[0].String(), port, data, conn)
+}
+
+func proxyThisCunt(ip string, port int, data []byte, browserConn net.Conn) {
+	dialing := ip + ":" + strconv.Itoa(port)
+	fmt.Println("Dialing this : ", dialing)
+	conn, err := net.Dial("tcp", dialing)
+
+	if err != nil {
+		panic(err)
+	}
+
+	conn.Write(data)
+
+	bs := make([]byte, 8192)
+	_, _ = conn.Read(bs)
+	fmt.Println(string(bs))
+	browserConn.Write(bs)
+	conn.Close()
+	browserConn.Close()
+}
