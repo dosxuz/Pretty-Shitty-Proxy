86631d63b6617e8633fa450186d9149b2fe092f8
dosuxz <ritabandas2000@gmail.com>
2020-10-21 23:18:46 -0700

    Cleaned up the code a bit

--- psp.go
+++ psp.go
@@ -36,9 +36,9 @@ func main() {
 
 func connString(conn net.Conn, data []byte) {
 	temp := strings.Split(string(data), "\n")
-	fmt.Println("Splitlines : ", len(temp))
-	fmt.Println("Of tyep : ", reflect.TypeOf(temp))
-	fmt.Println("Printing the array...")
+	//fmt.Println("Splitlines : ", len(temp))
+	//fmt.Println("Of tyep : ", reflect.TypeOf(temp))
+	//fmt.Println("Printing the array...")
 	var addr string
 	var tt string
 	var port int
@@ -71,7 +71,7 @@ func connString(conn net.Conn, data []byte) {
 
 func proxyThisCunt(ip string, port int, data []byte, browserConn net.Conn) {
 	dialing := ip + ":" + strconv.Itoa(port)
-	fmt.Println("Dialing this : ", dialing)
+	//fmt.Println("Dialing this : ", dialing)
 	conn, err := net.Dial("tcp", dialing)
 
 	if err != nil {
